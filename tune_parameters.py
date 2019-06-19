8bf46f7e507bbb84b03ca82b07ca40aa0d3bbc64
GuardSkill <qq497291093@gmail.com>
2019-06-19 22:57:36 +0800

    update tuneparameters

--- tune_parameters.py
+++ tune_parameters.py
@@ -5,7 +5,7 @@ import os
 # do your thing with the hyper-parameters
 from src.edge_connect import EdgeConnect
 
-
+TRAIN_LOSS=False
 def randomTune(config):
     # 'LR': 0.0001,                   # learning rate
     # 'D2G_LR': 0.1,                  # discriminator/generator learning rate ratio
@@ -13,32 +13,56 @@ def randomTune(config):
     # 'BETA2': 0.9,                   # adam optimizer beta2
     # 'L1_LOSS_WEIGHT': 1,            # l1 loss weight
     # 'FM_LOSS_WEIGHT': 10,           # feature-matching loss weight
-    config.MAX_STEPS=200
-    config.EVAL_INTERVAL=100
+    config.MAX_STEPS = 1000
+    config.EVAL_INTERVAL = 80
     config.MAX_EPOCHES = 1
     # config.MAX_STEPS = 3
     experiments = 50
     for i in range(experiments):
         # sample from a Uniform distribution on a log-scale
-        # config.LR = 10 ** np.random.uniform(-3, -5)  # Sample learning rate candidates in the range (0.0001 to 0.001)
+        config.LR = 10 ** np.random.uniform(-3, -5)  # Sample learning rate candidates in the range (0.001 to 0.00001)
+        config.D2G_LR = 10 ** np.random.uniform(-2,
+                                                0)  # Sample regularization candidates in the range (0.01 to 0.0001)
+        # config.LR = 0.0001
+        # config.D2G_LR =0.1
         # # config.LR=0.0001
-        # config.D2G_LR = 10 ** np.random.uniform(-2, -4)  # Sample regularization candidates in the range (0.001 to 0.01)
+
         # config.PATH = './checkpoints/places2_tune_%d_%f%f_' % (i, config.LR, config.D2G_LR)
         # logdir= config.PATH+('/log_%s_%s' % (config.LR , config.D2G_LR))
         create_dir(config.PATH)
-        if config.MODEL!=1:
-            config.L1_LOSS_WEIGHT = 10 ** np.random.uniform(-1,1)  # Sample regularization candidates in the range (1 to 200)
-            config.FM_LOSS_WEIGHT = 10 ** np.random.uniform(-1,1.5)  # Sample regularization candidates in the range (1 to 200)
-            # config.STYLE_LOSS_WEIGHT = 2 * 10 ** np.random.uniform(0,2)  # Sample regularization candidates in the range (1 to 200)
-            # config.CONTENT_LOSS_WEIGHT = 2 * 10 ** np.random.uniform(0,2)  # Sample regularization candidates in the range (1 to 200)
-            config.ADV_LOSS_WEIGHT = 10 ** np.random.uniform(-1,1)  # Sample regularization candidates in the range (1 to 200)
+        if TRAIN_LOSS:
+            if config.MODEL == 1:
+                config.L1_LOSS_WEIGHT = 10 ** np.random.uniform(-1,
+                                                                1)  # Sample regularization candidates in the range (1 to 200)
+                config.FM_LOSS_WEIGHT = 10 ** np.random.uniform(-1,
+                                                                1.5)  # Sample regularization candidates in the range (1 to 200)
+                config.ADV_LOSS_WEIGHT = 10 ** np.random.uniform(-1,
+                                                                 1)  # Sample regularization candidates in the range (1 to 200)
+            if config.MODEL != 1:
+                config.L1_LOSS_WEIGHT = 10 ** np.random.uniform(-1,
+                                                                1)  # Sample regularization candidates in the range (1 to 200)
+                config.FM_LOSS_WEIGHT = 10 ** np.random.uniform(-1,
+                                                                1.5)  # Sample regularization candidates in the range (1 to 200)
+                config.STYLE_LOSS_WEIGHT = 2 * 10 ** np.random.uniform(0,
+                                                                       2)  # Sample regularization candidates in the range (1 to 200)
+                config.CONTENT_LOSS_WEIGHT = 2 * 10 ** np.random.uniform(0,
+                                                                         2)  # Sample regularization candidates in the range (1 to 200)
+                config.ADV_LOSS_WEIGHT = 10 ** np.random.uniform(-1,
+                                                                 1)  # Sample regularization candidates in the range (1 to 200)
         model = EdgeConnect(config)
         model.load()
         # config.print()
         print('\nEx %d: learning_rate:%f  D_Learning_rate: %f:' % (i, config.LR, config.D2G_LR))
-        print('\nEx %d: L1:%f  FM: %f  ADV: %f:' % (i, config.L1_LOSS_WEIGHT,config.FM_LOSS_WEIGHT,config.STYLE_LOSS_WEIGHT ))
-        # print('\nEx %d: L1:%f  FM: %f  STYLE: %f CONTENT: %f ADV: %f:' % (i, config.L1_LOSS_WEIGHT,
-        #      config.FM_LOSS_WEIGHT,config.STYLE_LOSS_WEIGHT, config.CONTENT_LOSS_WEIGHT,config.STYLE_LOSS_WEIGHT ))
+        if TRAIN_LOSS:
+            if config.MODEL == 1:
+                print('Ex %d: L1:%f  FM: %f  ADV: %f:' % (
+                    i, config.L1_LOSS_WEIGHT, config.FM_LOSS_WEIGHT, config.ADV_LOSS_WEIGHT))
+            if config.MODEL != 1:
+                print('Ex %d: L1:%f  FM: %f  STYLE: %f CONTENT: %f ADV: %f:' % (i, config.L1_LOSS_WEIGHT,
+                                                                                config.FM_LOSS_WEIGHT,
+                                                                                config.STYLE_LOSS_WEIGHT,
+                                                                                config.CONTENT_LOSS_WEIGHT,
+                                                                                config.STYLE_LOSS_WEIGHT))
 
         model.train()
 
